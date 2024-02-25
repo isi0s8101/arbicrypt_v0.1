8e01cfa4644ee15aadfc9681ae555d92216a9543
!$i0S_101 <159912951+isi0s8101@users.noreply.github.com>
2024-02-25 13:28:54 +0100

    Update1 Profile.vue
    Un composant pour la page de profil de l'utilisateur, similaire au tableau de bord en structure.

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Profile</h1>
+    <p>This is the Profile component.</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+};
+</script>
